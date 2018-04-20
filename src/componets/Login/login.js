dc912734d2e0aef128814896bdf094aa57a001c0
Edgar Garcia <edgargarcia@edgars-mbp.ibmmobiledemo.com>
2018-04-20 15:25:40 -0500

    Login
    Funciona la pnatalla de manera basica, falta registro

--- src/componets/Login/login.js
+++ src/componets/Login/login.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Image} from 'react-native'
+import React, { Component } from 'react';
+import { StyleSheet, View, Image, keyboardAvoidingView} from 'react-native';
+import LoginForm from './LoginForms';
 
 export default class Login extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={style.logoContainer}>
-          <Image source={}/>
-        </View>
-        <View style={style.formContainer}>
+      <KeyboardAvoidingView benhavior="padding" style={styles.container}>
+        <View style={styles.container}>
+          <View style={style.logoContainer}>
+            <Image
+              source={required('../../Images/logoRestaurante.png')}
+            />
+            <text style={styles.title}>Una app de restaurante hecha en React Native </text>
+          </View>
+          <View style={style.formContainer}>
         </View>
       </View>
-    )
+    </KeyboardAvoidingView>
+  );
   }
 }
 
@@ -19,5 +25,21 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: '#3498db'
+    },
+    logoContainer:{
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center'
+    },
+    logo:{
+      width: 100,
+      heigth: 1000
+    },
+    title:{
+      color:'#FFF'
+      marginTop: 10,
+      width: 160,
+      textAling: 'center',
+      opacity: 0.8
     }
 });
